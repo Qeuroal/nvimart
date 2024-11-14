8dedc6057153b6662ac68d0c17493bb44b74a199
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-11-14 22:14:32 +0800

    transparent

--- lua/plugin/plugins.lua
+++ lua/plugin/plugins.lua
@@ -261,6 +261,19 @@ config.neogit = {
 --     },
 -- }
 
+-- config["nvim-scrollview"] = {
+--     "dstein64/nvim-scrollview",
+--     event = "User Loading",
+--     main = "scrollview",
+--     opts = {
+--         excluded_filetypes = { "nvimtree" },
+--         current_only = true,
+--         winblend = 75,
+--         base = "right",
+--         column = 1,
+--     },
+-- }
+
 config.nui = {
     "MunifTanjim/nui.nvim",
     lazy = true,
@@ -270,6 +283,7 @@ config["nvim-autopairs"] = {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     main = "nvim-autopairs",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "hrsh7th/nvim-cmp" },
     opts = {},
 }
 
@@ -288,19 +302,6 @@ config["nvim-notify"] = {
     end,
 }
 
--- config["nvim-scrollview"] = {
---     "dstein64/nvim-scrollview",
---     event = "User Loading",
---     main = "scrollview",
---     opts = {
---         excluded_filetypes = { "nvimtree" },
---         current_only = true,
---         winblend = 75,
---         base = "right",
---         column = 1,
---     },
--- }
-
 config["nvim-transparent"] = {
     "xiyaowong/nvim-transparent",
     opts = {
@@ -310,38 +311,7 @@ config["nvim-transparent"] = {
         },
     },
     config = function(_, opts)
-        local autogroup = vim.api.nvim_create_augroup("transparent", { clear = true })
-        vim.api.nvim_create_autocmd("ColorScheme", {
-            group = autogroup,
-            callback = function()
-                local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
-                local foreground = string.format("#%06x", normal_hl.fg)
-                local background = string.format("#%06x", normal_hl.bg)
-                vim.api.nvim_command("highlight default NvimartNormal guifg=" .. foreground .. " guibg=" .. background)
-
-                require("transparent").clear()
-            end,
-        })
-        -- Enable transparent by default
-        local transparent_cache = vim.fn.stdpath "data" .. "/transparent_cache"
-        if not require("utils.basic").file_exists(transparent_cache) then
-            local f = io.open(transparent_cache, "w")
-            f:write "true"
-            f:close()
-        end
-
-        require("transparent").setup(opts)
-
-        local old_get_hl = vim.api.nvim_get_hl
-        vim.api.nvim_get_hl = function(ns_id, opt)
-            if opt.name == "Normal" then
-                local attempt = old_get_hl(0, { name = "NvimartNormal" })
-                if next(attempt) ~= nil then
-                    opt.name = "NvimartNormal"
-                end
-            end
-            return old_get_hl(ns_id, opt)
-        end
+        require("plugin.config.nvim_transparent").setup(_, opts)
     end,
 }
 
