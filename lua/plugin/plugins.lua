e826ccd34ee0ff5294e7496987bb689e69c9c343
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-11-14 21:46:00 +0800

    comment

--- lua/plugin/plugins.lua
+++ lua/plugin/plugins.lua
@@ -81,6 +81,7 @@ config.bufferline = {
     },
 }
 
+-- 高亮 RGB 颜色
 config.colorizer = {
     "NvChad/nvim-colorizer.lua",
     main = "colorizer",
@@ -112,61 +113,10 @@ config.comment = {
         mappings = { basic = true, extra = true, extended = false },
     },
     config = function(_, opts)
-        require("Comment").setup(opts)
-
-        -- Remove the keymap defined by Comment.nvim
-        vim.keymap.del("n", "gcc")
-        vim.keymap.del("n", "gbc")
-        vim.keymap.del("n", "gc")
-        vim.keymap.del("n", "gb")
-        vim.keymap.del("x", "gc")
-        vim.keymap.del("x", "gb")
-        vim.keymap.del("n", "gcO")
-        vim.keymap.del("n", "gco")
-        vim.keymap.del("n", "gcA")
+        require("plugin.config.comment").setup(_, opts)
     end,
     keys = function()
-        local vvar = vim.api.nvim_get_vvar
-
-        local toggle_current_line = function()
-            if vvar "count" == 0 then
-                return "<Plug>(comment_toggle_linewise_current)"
-            else
-                return "<Plug>(comment_toggle_linewise_count)"
-            end
-        end
-
-        local toggle_current_block = function()
-            if vvar "count" == 0 then
-                return "<Plug>(comment_toggle_blockwise_current)"
-            else
-                return "<Plug>(comment_toggle_blockwise_count)"
-            end
-        end
-
-        local comment_below = function()
-            require("Comment.api").insert.linewise.below()
-        end
-
-        local comment_above = function()
-            require("Comment.api").insert.linewise.above()
-        end
-
-        local comment_eol = function()
-            require("Comment.api").locked "insert.linewise.eol"()
-        end
-
-        return {
-            { "<leader>c", "<Plug>(comment_toggle_linewise)", desc = "comment toggle linewise" },
-            { "<leader>ca", "<Plug>(comment_toggle_blockwise)", desc = "comment toggle blockwise" },
-            { "<leader>cc", toggle_current_line, expr = true, desc = "comment toggle current line" },
-            { "<leader>cb", toggle_current_block, expr = true, desc = "comment toggle current block" },
-            { "<leader>cc", "<Plug>(comment_toggle_linewise_visual)", mode = "x", desc = "comment toggle linewise" },
-            { "<leader>cb", "<Plug>(comment_toggle_blockwise_visual)", mode = "x", desc = "comment toggle blockwise" },
-            { "<leader>co", comment_below, desc = "comment insert below" },
-            { "<leader>cO", comment_above, desc = "comment insert above" },
-            { "<leader>cA", comment_eol, desc = "comment insert end of line" },
-        }
+        return require("plugin.config.comment").setKeymapping()
     end,
 }
 
@@ -879,6 +829,7 @@ config["nvim-tmux-navigation"] = {
     end,
 }
 
+-- !!! 需要补充快捷键
 -- config["hop"] = {
 --     'phaazon/hop.nvim',
 --     branch = 'v2',      -- optional but strongly recommended
