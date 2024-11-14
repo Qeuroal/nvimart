eff478b9a9739833368b2c62b4b1fac2f8f87bff
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-11-14 18:44:43 +0800

    modify

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -4,6 +4,7 @@ function M:init()
     -- must first line
     gvimconf = vim.deepcopy(require("default"))
 
+    -- plugin config bootstrap
     local plugins = require("plugin.config.bootstrap")
     plugins.config({})
 end
--- lua/color/init.lua
+++
@@ -1,8 +0,0 @@
--- define your colorscheme here
-local colorscheme = 'onedark'
-
-local isOk, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not isOk then
-    vim.notify('colorscheme ' .. colorscheme .. ' not found!')
-    return
-end
