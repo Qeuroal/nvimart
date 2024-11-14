d0ce9b2cdbbf60ee928e55773ac261673338a84d
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-11-14 21:36:21 +0800

    hop

--- lua/plugin/plugins.lua
+++ lua/plugin/plugins.lua
@@ -879,6 +879,15 @@ config["nvim-tmux-navigation"] = {
     end,
 }
 
+-- config["hop"] = {
+--     'phaazon/hop.nvim',
+--     branch = 'v2',      -- optional but strongly recommended
+--     config = function()
+--         -- you can configure Hop the way you like here; see :h hop-config
+--         require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+--     end,
+-- }
+
 -- Colorschemes
 config["ayu"] = {
     "Luxed/ayu-vim",
