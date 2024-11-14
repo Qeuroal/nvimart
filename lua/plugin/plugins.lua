038ccb5e384ab0c36b7f32e71fff937408e732a9
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-11-14 22:33:28 +0800

    which key

--- lua/plugin/plugins.lua
+++ lua/plugin/plugins.lua
@@ -423,44 +423,7 @@ config.undotree = {
 config["which-key"] = {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    opts = {
-        icons = {
-            mappings = false,
-        },
-        plugins = {
-            marks = true,
-            registers = true,
-            spelling = {
-                enabled = false,
-            },
-            presets = {
-                operators = false,
-                motions = true,
-                text_objects = true,
-                windows = true,
-                nav = true,
-                z = true,
-                g = true,
-            },
-        },
-        spec = {
-            { "<leader>b", group = "+buffer" },
-            { "<leader>c", group = "+comment" },
-            { "<leader>g", group = "+git" },
-            { "<leader>h", group = "+hop" },
-            { "<leader>l", group = "+lsp" },
-            { "<leader>t", group = "+telescope" },
-            { "<leader>u", group = "+utils" },
-        },
-        win = {
-            border = "none",
-            padding = { 1, 0, 1, 0 },
-            wo = {
-                winblend = 0,
-            },
-            zindex = 1000,
-        },
-    },
+    opts = require("plugin.config.which_key").opts(),
 }
 
 config["zen-mode"] = {
