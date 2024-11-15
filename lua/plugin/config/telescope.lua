94ec4f1b1b2ae470eb9f075be195a5decaf18d29
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-11-15 15:34:29 +0800

    telescope

--- lua/plugin/config/telescope.lua
+++ lua/plugin/config/telescope.lua
@@ -14,32 +14,104 @@ function M.dependencies()
 end
 
 function M.opts()
+    local actions = require("utils.modules").requireOnExportedCall "telescope.actions"
     return {
+        ---@usage disable telescope completely [not recommended]
+        active = true,
+        on_config_done = nil,
+        theme = "dropdown", ---@type telescope_themes
         defaults = {
+            prompt_prefix = gvimconf.icons.ui.Telescope .. " ",
+            selection_caret = gvimconf.icons.ui.Forward .. " ",
+            entry_prefix = "  ",
             initial_mode = "insert",
+            selection_strategy = "reset",
+            sorting_strategy = nil,
+            layout_strategy = nil,
+            layout_config = {},
+            vimgrep_arguments = {
+                "rg",
+                "--color=never",
+                "--no-heading",
+                "--with-filename",
+                "--line-number",
+                "--column",
+                "--smart-case",
+                "--hidden",
+                "--glob=!.git/",
+            },
+            ---@usage Mappings are fully customizable. Many familiar mapping patterns are setup as defaults.
             mappings = {
                 i = {
-                    ["<C-j>"] = "move_selection_next",
-                    ["<C-k>"] = "move_selection_previous",
-                    ["<C-n>"] = "cycle_history_next",
-                    ["<C-p>"] = "cycle_history_prev",
-                    ["<C-c>"] = "close",
-                    ["<C-u>"] = "preview_scrolling_up",
-                    ["<C-d>"] = "preview_scrolling_down",
+                    ["<C-j>"] = actions.move_selection_next,
+                    ["<C-k>"] = actions.move_selection_previous,
+                    ["<C-c>"] = actions.close,
+                    ["<C-n>"] = actions.cycle_history_next,
+                    ["<C-p>"] = actions.cycle_history_prev,
+                    ["<C-q>"] = function(...)
+                        actions.smart_send_to_qflist(...)
+                        actions.open_qflist(...)
+                    end,
+                    ["<CR>"] = actions.select_default,
+                },
+                n = {
+                    ["<C-n>"] = actions.move_selection_next,
+                    ["<C-p>"] = actions.move_selection_previous,
+                    ["<C-c>"] = actions.close,
+                    ["<C-q>"] = function(...)
+                        actions.smart_send_to_qflist(...)
+                        actions.open_qflist(...)
+                    end,
                 },
             },
+            file_ignore_patterns = {},
+            path_display = { "smart" },
+            winblend = 0,
+            border = {},
+            borderchars = nil,
+            color_devicons = true,
+            set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
         },
         pickers = {
             find_files = {
-                winblend = 20,
+                hidden = true,
+            },
+            live_grep = {
+                --@usage don't include the filename in the search results
+                only_sort_text = true,
+            },
+            grep_string = {
+                only_sort_text = true,
+            },
+            buffers = {
+                initial_mode = "normal",
+                mappings = {
+                    i = {
+                        ["<C-d>"] = actions.delete_buffer,
+                    },
+                    n = {
+                        ["dd"] = actions.delete_buffer,
+                    },
+                },
+            },
+            planets = {
+                show_pluto = true,
+                show_moon = true,
+            },
+            git_files = {
+                hidden = true,
+                show_untracked = true,
+            },
+            colorscheme = {
+                enable_preview = true,
             },
         },
         extensions = {
             fzf = {
-                fuzzy = true,
-                override_generic_sorter = true,
-                override_file_sorter = true,
-                case_mode = "smart_case",
+                fuzzy = true, -- false will only do exact matching
+                override_generic_sorter = true, -- override the generic sorter
+                override_file_sorter = true, -- override the file sorter
+                case_mode = "smart_case", -- or "ignore_case" or "respect_case"
             },
         },
     }
