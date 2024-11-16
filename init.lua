71fc1e172ca1e8e3cde95724448b022c9a3a918a
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-11-16 11:58:53 +0800

    modify plugin

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ require("bootstrap"):init()
 require("config")
 require("plugin")
 
-local baseUtils = require("utils.basic")
+baseUtils = require("utils.basic")
 
 -- Load user configuration files
 local config_root = string.gsub(vim.fn.stdpath "config", "\\", "/")
@@ -13,7 +13,7 @@ end
 
 local custom_path = config_root .. "/lua/custom/"
 if baseUtils.file_exists(custom_path .. "init.lua") then
-    require("custom.init")
+    require "custom.init"
 end
 
 -- Define keymap
@@ -22,6 +22,34 @@ baseUtils.group_map(keymap)
 
 for filetype, config in pairs(gvimconf.ft) do
     baseUtils.ft(filetype, config)
+end
+
+-- Only load plugins and colorscheme when --noplugin arg is not present
+if not baseUtils.noplugin then
+    -- Load plugins
+    local config = {}
+    for _, plugin in pairs(gvimconf.plugin.list) do
+        config[#config + 1] = plugin
+    end
+    require("lazy").setup(config, gvimconf.lazy)
+
+    baseUtils.group_map(gvimconf.keymap.plugins)
+
+    -- Define colorscheme
+    if not gvimconf.colorscheme then
+        local colorscheme_cache = vim.fn.stdpath "data" .. "/colorscheme"
+        if baseUtils.file_exists(colorscheme_cache) then
+            local colorscheme_cache_file = io.open(colorscheme_cache, "r")
+            ---@diagnostic disable: need-check-nil
+            local colorscheme = colorscheme_cache_file:read "*a"
+            colorscheme_cache_file:close()
+            gvimconf.colorscheme = colorscheme
+        else
+            gvimconf.colorscheme = "onedark"
+        end
+    end
+
+    require("plugin.utils").colorscheme(gvimconf.colorscheme)
 end
 
 -- Prepend this to runtimepath last as it would be overridden by lazy otherwise
--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -5,32 +5,3 @@ require("plugin.plugins")
 require("plugin.colorscheme")
 require("plugin.keymappings")
 
--- Only load plugins and colorscheme when --noplugin arg is not present
-local baseUtils = require("utils.basic")
-if not baseUtils.noplugin then
-    -- Load plugins
-    local config = {}
-    for _, plugin in pairs(gvimconf.plugin.list) do
-        config[#config + 1] = plugin
-    end
-    require("lazy").setup(config, gvimconf.lazy)
-
-    baseUtils.group_map(gvimconf.keymap.plugins)
-
-    -- Define colorscheme
-    if not gvimconf.colorscheme then
-        local colorscheme_cache = vim.fn.stdpath "data" .. "/colorscheme"
-        if baseUtils.file_exists(colorscheme_cache) then
-            local colorscheme_cache_file = io.open(colorscheme_cache, "r")
-            ---@diagnostic disable: need-check-nil
-            local colorscheme = colorscheme_cache_file:read "*a"
-            colorscheme_cache_file:close()
-            gvimconf.colorscheme = colorscheme
-        else
-            gvimconf.colorscheme = "onedark"
-        end
-    end
-
-    require("plugin.utils").colorscheme(gvimconf.colorscheme)
-end
-
